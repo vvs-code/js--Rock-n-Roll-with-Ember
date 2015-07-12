f08c4285e4ebf2d02f6b0d8f8512f72bab28fdc1
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-12 20:45:08 +0300

    Git test for songs sorting. Implement sorting

--- app/pods/band/songs/controller.js
+++ app/pods/band/songs/controller.js
@@ -9,9 +9,25 @@ export default Ember.Controller.extend({
   noSongs: Ember.computed.empty('model'),
   canCreateSongs: Ember.computed.or('model.length', 'songCreationStarted'),
 
+  sortBy: 'ratingDesc',
+  sortProperties: function() {
+    var options = {
+      "ratingDesc": "rating:desc,title:asc",
+      "ratingAsc": "rating:asc,title:asc",
+      "titleDesc": "title:desc",
+      "titleAsc": "title:asc",
+    };
+    return options[this.get('sortBy')].split(',');
+  }.property('sortBy'),
+  sortedSongs: Ember.computed.sort('model', 'sortProperties'),
+
   actions: {
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
+    },
+
+    setSorting: function(option) {
+      this.set('sortBy', option);
     }
   }
 });
--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -101,3 +101,51 @@ test('Create a new song in two steps', function(assert) {
     assert.equal(find('.songs .song:contains("Killer Cars")').length, 1, "Creates the song and displays it in the list");
   });
 });
+
+test('Sort songs in various ways', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function(request) {
+      var resp = JSON.stringify({
+        bands: [
+          {
+            id: 1,
+            name: 'Them Crooked Vultures',
+            songs: [1, 2, 3, 4] }
+        ],
+        songs: [
+          { id: 1, title: 'Elephants', rating: 5 },
+          { id: 2, title: 'New Fang', rating: 4 },
+          { id: 3, title: 'Mind Eraser, No Chaser', rating: 4 },
+          { id: 4, title: 'Spinning in Daffodils', rating: 5 }
+        ]
+      });
+
+      return [200, { "Content-Type": "application/json" }, resp];
+
+    });
+  });
+
+  visit('/bands/1/songs');
+
+  andThen(function() {
+    //assert.equal(currentURL(), '/bands/1/songs');
+    assertTrimmedText(assert, '.song:first', 'Elephants by Them Crooked Vultures', "The first song is the highest ranked, first in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'New Fang by Them Crooked Vultures', "The last song is the lowest ranked, last in the alphabet");
+  });
+
+  click('button.sort-title-desc');
+
+  andThen(function() {
+    //assert.equal(currentURL(), '/bands/1/songs?sort=titleDesc');
+    assertTrimmedText(assert, '.song:first', 'Spinning in Daffodils by Them Crooked Vultures', "The first song is the one that is the last in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'Elephants by Them Crooked Vultures', "The last song is the one that is the first in the alphabet");
+  });
+
+  click('button.sort-rating-asc');
+
+  andThen(function() {
+    //assert.equal(currentURL(), '/bands/1/songs?sort=ratingAsc');
+    assertTrimmedText(assert, '.song:first', 'Mind Eraser, No Chaser by Them Crooked Vultures', "The first song is the lowest ranked, first in the alphabet");
+    assertTrimmedText(assert, '.song:last', 'Spinning in Daffodils by Them Crooked Vultures', "The last song is the highest ranked, last in the alphabet");
+  });
+});
