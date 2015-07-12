f337039e86757c7b053a33a732e5d116cda074a6
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-12 20:11:41 +0300

    Add star-rating component test

--- tests/unit/pods/components/star-rating/component-test.js
+++ tests/unit/pods/components/star-rating/component-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 
 moduleForComponent('star-rating', 'Unit | Component | star rating', {
@@ -16,4 +17,22 @@ test('it renders', function(assert) {
   // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+
+test('Renders the full and empty stars correctly', function(assert) {
+  var component = this.subject();
+  Ember.run(function() {
+    component.setProperties({
+      rating: 4,
+      maxRating: 5
+    });
+  });
+  assert.equal(this.$().find('.glyphicon-star').length, 4, "The right amount of full stars is rendered");
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 1, "The right amount of empty stars is rendered");
+  Ember.run(function() {
+    component.set('maxRating', 10);
+  });
+  assert.equal(this.$().find('.glyphicon-star').length, 4, "The right amount of full stars is rendered after changing maxRating");
+  assert.equal(this.$().find('.glyphicon-star-empty').length, 6, "The right amount of empty stars is rendered after changing maxRating");
 });
