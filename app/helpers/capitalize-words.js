3413de971b1d22c60ac2c680ebaee17bce37a909
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-12 21:21:28 +0300

    Add capitalize helper

---
+++ app/helpers/capitalize-words.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export function capitalizeWords(input/*, hash*/) {
+  var words = input.toString().split(/\s+/).map(function(word) {
+    return word.toLowerCase().capitalize(); });
+  return words.join(' ');
+}
+
+export default Ember.HTMLBars.makeBoundHelper(capitalizeWords);
---
+++ tests/unit/helpers/capitalize-words-test.js
@@ -0,0 +1,10 @@
+import { capitalizeWords } from '../../../helpers/capitalize-words';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | capitalize words');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = capitalizeWords(42);
+  assert.ok(result);
+});
