6d04c5570ead1fd8ade324620d7e8be3845e05b2
Vasil Vanchuk <v.vanchuk@tut.by>
2015-07-12 12:55:06 +0300

    Some UX emprovements - create on submit. Default disabled songs creation

--- app/pods/band/songs/controller.js
+++ app/pods/band/songs/controller.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
   title: '',
 
-  isAddButtonDisabled: Ember.computed.empty('title')
+  isAddButtonDisabled: Ember.computed.empty('title'),
+
+  noSongs: Ember.computed.empty('model'),
+  canCreateSongs: Ember.computed.or('model.length', 'songCreationStarted'),
+
+  actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    }
+  }
 });
